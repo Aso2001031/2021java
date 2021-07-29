c7e80b963656458b1350d27b4333f1cad7c82a86
Aso2001031 <83050649+Aso2001031@users.noreply.github.com>
2021-07-29 11:11:36 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+
+        //ゲーム初期化
+        game.init();
+        //ゲーム開始
+        game.execute();
+    }
+}
